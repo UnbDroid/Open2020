52b207951300fa6e27eb53552c507502c2a8c961
Rodrigo Zamagno <31426233+DigoZM@users.noreply.github.com>
2020-10-02 18:10:33 -0300

    Update sensor_cor.lua
    Adicionado função que retorna a cor do sensor

--- scripts/sensores/sensor_cor.lua
+++ scripts/sensores/sensor_cor.lua
@@ -1,11 +1,42 @@
 function sysCall_init()
     visionSensor=simGetObjectHandle('Vision_sensor') -- nome do sensor
+    BLACK = 0
+    RED = 1
+    YELLOW = 2
+    GREEN = 3
+    BLUE = 4
+    WHITE = 5
+end
+
+function getColorSensor(sensor)
+    local image=simGetVisionSensorCharImage(visionSensor)  -- Visão com baixa resolução (pixel 1x1) 
+    local redPixel=image:byte(1) -- Valores em escala RGB: [0-173]
+    local greenPixel=image:byte(2)
+    local bluePixel=image:byte(3)
+    local minColor = 150
+    local red, green, blue, colorRGB
+    red = 0
+    green = 0
+    blue = 0
+    if (redPixel > minColor) then 
+        red = 1
+    end
+    if (greenPixel > minColor) then 
+        green = 10
+    end
+    if (bluePixel > minColor) then 
+        blue = 100
+    end
+    colorRGB = red + green + blue
+    if     (colorRGB == 0) then return BLACK
+    elseif (colorRGB == 1) then return RED
+    elseif (colorRGB == 11) then return YELLOW
+    elseif (colorRGB == 10) then return GREEN
+    elseif (colorRGB == 100) then return BLUE
+    elseif (colorRGB == 111) then return BLACK
+    end
 end
 
 function sysCall_sensing()
-  local image=simGetVisionSensorCharImage(visionSensor) -- Visão com baixa resolução (pixel 1x1) 
-	local redPixel=image:byte(1) -- Valores em escala RGB: [0-173]
-	local greenPixel=image:byte(2)
-	local bluePixel=image:byte(3)
-  print(redPixel, greenPixel, bluePixel )
+    print(getColorSensor(visionSensor))
 end
